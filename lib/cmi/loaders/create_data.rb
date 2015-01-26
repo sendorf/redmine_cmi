ab447fedcb909612c232c8f8d6321aae16e38392
Sergio Díaz <sergio.diaz@mo2o.com>
2015-01-26 15:57:27 +0100

    Try moving update profiles to observer to load role cost history rake task

--- lib/cmi/loaders/create_data.rb
+++ lib/cmi/loaders/create_data.rb
@@ -6,15 +6,6 @@ module CMI
         UserCustomField.create(:type => "UserCustomField", :name => DEFAULT_VALUES['user_role_field'],
                                :field_format => "list", :possible_values => ['Project Manager', 'Functional Analyst', 'Analyst Developer', 'Senior Developer', 'Junior Developer', 'Scholarship', 'Observer'],
                                :regexp => "", :is_required => true, :is_for_all => false, :is_filter => false, :searchable => false, :editable => false, :default_value => nil)
-
-        total_users = User.all.count
-    
-        ((total_users/500).to_i + 1).times do |iterator|
-          users = User.limit(500).offset((iterator*500))
-          users.each do |user|
-            user.role = 'Observer'
-          end
-        end
       end
     end
   end
--- lib/tasks/load_role_costs_history.rake
+++ lib/tasks/load_role_costs_history.rake
@@ -6,5 +6,13 @@ desc 'Load CMI role costs history. (db/fixtures/history_profiles_costs.csv)'
 namespace :cmi do
   task :load_role_costs_history => :environment do
     ActiveRecord::Fixtures.create_fixtures(File.join(File.dirname(__FILE__), %w[.. .. db fixtures]), 'history_profiles_costs')
+    total_users = User.all.count
+    
+    ((total_users/500).to_i + 1).times do |iterator|
+      users = User.limit(500).offset((iterator*500))
+      users.each do |user|
+        user.role = 'Observer'
+      end
+    end
   end
 end
